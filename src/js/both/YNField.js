50c8201fa597bc10cdb74b7719781bcff15dc4f5
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-09-24 21:01:29 +0200

    add serializer

--- src/js/both/YNField.js
+++ src/js/both/YNField.js
@@ -5,5 +5,10 @@ Ext.define('Tualo.field.YNField', {
         if (v === 'Y') return true;
         if (v === 'N') return false;
         return this.callParent([v, record]);
+    },
+    serialize: function (v, record) {
+        if (v === true) return 'Y';
+        if (v === false) return 'N';
+        return this.callParent([v, record]);
     }
 });
